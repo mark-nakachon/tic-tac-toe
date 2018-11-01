a14aea30b7b08b9a3e36c6aa77fb4d8c74e03251
mark-nakachon <42904380+mark-nakachon@users.noreply.github.com>
2018-11-01 16:25:16 +0530

    written functions to understand what to do
    inital step

---
+++ tic.py
@@ -0,0 +1,25 @@
+def insert_letter(letter,position):
+    pass
+def space_is_free(position):
+    pass
+
+def print_board(board):
+    pass
+def is_winner():
+    pass
+
+def player_move():
+    pass
+def comp_move();
+    pass
+
+def select_random():
+    pass
+def is_full():
+    pass
+
+
+def main():
+    pass
+
+main()
