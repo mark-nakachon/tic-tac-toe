65607815daac33396dd797a370a548e0c4730871
mark-nakachon <42904380+mark-nakachon@users.noreply.github.com>
2018-11-01 16:47:36 +0530

    added functions

--- tic.py
+++ tic.py
@@ -1,12 +1,26 @@
-def insert_letter(letter,position):
-    pass
-def space_is_free(position):
-    pass
+board=[' ' for i in range(10)]
 
+def insert_letter(letter,position):
+    board[position] = letter
+def space_is_free(position):
+    if board[position] == ' ':
+        return True
+    else:
+        return False
 def print_board(board):
-    pass
-def is_winner():
-    pass
+    print("  |  |  ")
+    print(board[1]," |",board[2]," |",board[3],sep="")
+    print("---------")
+    print("  |  |  ")
+    print(board[4]," |",board[5]," |",board[6],sep="")
+    print("---------")
+    print("  |  |  ")
+    print(board[7]," |",board[8]," |",board[9],sep="")
+    print("---------")
+    
+
+def is_winner(symbol):
+    if (board[1] == symbol and board[2] == symbol and board[3] == symbol) or(board[4] == symbol)
 
 def player_move():
     pass
@@ -20,6 +34,12 @@ def is_full():
 
 
 def main():
-    pass
+    
 
-main()
+
+game_state = True
+while game_state:
+    main()
+    a=input('do u want to play again\y\n')
+    if a == n:
+        game_state = False
